6dc3a07689e0bdcfb3a91d50537770de18668957
kazpot <knman7@hotmail.com>
2017-06-19 00:43:01 +0900

    Try tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,13 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 10;
-double dt = 0.1;
+
+//N=10, dt=0.1
+//N=20, dt=0.1 worse 
+//N=10, dt=0.05 still bad
+//N=20, dt=0.05
+size_t N = 20;
+double dt = 0.05;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -50,21 +55,21 @@ class FG_eval {
 
     //any anything you think may be beneficial.
     for (int t = 0; t < N; t++) {
-        fg[0] += CppAD::pow(vars[cte_start + t], 2);
-        fg[0] += CppAD::pow(vars[epsi_start + t], 2);
+        fg[0] += 2000 * CppAD::pow(vars[cte_start + t], 2);
+        fg[0] += 2000 * CppAD::pow(vars[epsi_start + t], 2);
         fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     //minimize the use of actuators.
     for (int t = 0; t < N - 1; t++) {
-        fg[0] += CppAD::pow(vars[delta_start + t], 2);
-        fg[0] += CppAD::pow(vars[a_start + t], 2);
+        fg[0] += 300 * CppAD::pow(vars[delta_start + t], 2);
+        fg[0] += 300 * CppAD::pow(vars[a_start + t], 2);
     }
 
     //minimize the value gap between sequential actuations.
     for (int t = 0; t < N - 2; t++) {
-        fg[0] += CppAD::pow(vars[delta_start + t +1] - vars[delta_start + t], 2);
-        fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+        fg[0] += 100 * CppAD::pow(vars[delta_start + t +1] - vars[delta_start + t], 2);
+        fg[0] += 100 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
     //initial constraints
